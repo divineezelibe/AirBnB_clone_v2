ad34b659ccb78c1bdd741e72a2e41dfa208639f5
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-07-10 02:17:27 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,29 +22,26 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
+    # Upload archive to remote /tmp directory
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
 
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    # Execute commands with elevated privileges using sudo
+    if sudo("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-    if sudo("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if sudo("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+    if sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if sudo("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+    if sudo("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+    if sudo("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if sudo("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    if sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
         return False
+
     return True
