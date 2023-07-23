b0b1e5096adff5ac41f920e0685af24a8f82e9ae
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-07-24 00:39:30 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,6 +10,7 @@ from models.state import State
 from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import MySQLdb
 
 
 class DBStorage:
